1b2ff395f07aea43b8e436839e4e7d8482fe18dd
Kaleb Wondwossen <kaleb.wond@gmail.com>
2023-04-19 21:59:29 -0500

    key bug fix

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Comments({comments}) {
   return (
     <ul className="ml-10">
-        {comments.map(comment => <li>{comment.name}</li>)}
+        {comments.map(comment => <li key={comment.id}>{comment.name}</li>)}
     </ul>
   )
 }
